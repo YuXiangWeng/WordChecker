93e11363ab190a4519cdd4e534409e684ebe3b48
YuXiangWeng <yuxiangw4@nycstudents.net>
2024-10-11 15:44:04 +0000

    Tried adding words

--- WordChecker.java
+++ WordChecker.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
+
 public class WordChecker {
     /** Initialized in the constructor and contains no null elements */
     private ArrayList<String> wordList;
+
+    public WordChecker(ArrayList<String> list) {
+        wordList = list;
+
+    }
+
+    public WordChecker() {
+        wordList = new ArrayList<String>();
+    }
 
     /**
      * Returns true if each element of wordList (except the first) contains the
